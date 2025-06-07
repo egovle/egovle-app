2ac238972fb4d0447b214b5f0a92030efce7c70a
Aditya Patkar <nuteniqspl@gmail.com>
2025-06-08 00:06:47 +0530

    Added working login page with Firebase

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
-import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>eGoVLE</title>
-        <meta name="description" content="eGoVLE - Tumche Sahayak" />
-      </Head>
-      <main>
-        <h1>Welcome to eGoVLE</h1>
-        <p>This is a starter for the customer, VLE and admin portal.</p>
-      </main>
-    </div>
-  )
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/login');
+  }, []);
+  return null;
 }
