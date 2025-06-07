4dcd0bcbb143f1bde5f649fa7429946e0705529c
Aditya Patkar <nuteniqspl@gmail.com>
2025-06-07 22:22:41 +0530

    Initial commit for eGoVLE

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>eGoVLE</title>
+        <meta name="description" content="eGoVLE - Tumche Sahayak" />
+      </Head>
+      <main>
+        <h1>Welcome to eGoVLE</h1>
+        <p>This is a starter for the customer, VLE and admin portal.</p>
+      </main>
+    </div>
+  )
+}
